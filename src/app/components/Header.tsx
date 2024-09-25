9a918e45661b7a4e5c52f2207487d7248459f287
Jo <johj703@nate.com>
2024-09-25 23:35:09 +0900

    add: Header 컴포넌트 생성

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
